0acf08318fd82dacc16478075e6bcdcc7ffba087
bohdanlazurenko <142092646+bohdanlazurenko@users.noreply.github.com>
2025-10-19 01:28:14 +0200

    Add tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'flip': 'flip 1s ease-in-out',
+        'spin-slow': 'spin 3s linear infinite',
+      },
+      keyframes: {
+        flip: {
+          '0%': { transform: 'rotateY(0deg)' },
+          '100%': { transform: 'rotateY(1800deg)' },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
